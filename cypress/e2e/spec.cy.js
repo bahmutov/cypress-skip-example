6c5da03f80e9589ac2350f1c4bcdea21b5386e5a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-09-04 09:34:31 -0400

    working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,33 @@
 /// <reference types="cypress" />
 
+// @ts-ignore "cy.state" is not in the "cy" type
+const getMochaContext = () => cy.state('runnable').ctx
+const skipIfPreviousTestsFailed = () => {
+  const ctx = getMochaContext()
+  const thisTestIndex = ctx.test.parent?.tests?.indexOf(ctx.test)
+  console.log(thisTestIndex)
+  const previousTests = ctx.test.parent?.tests?.slice(
+    0,
+    thisTestIndex,
+  )
+  console.log(previousTests)
+  const anyFailedTests = previousTests?.some(
+    (test) => test.state === 'failed',
+  )
+  console.log({ thisTestIndex, anyFailedTests })
+  if (anyFailedTests) {
+    return ctx.skip()
+  }
+}
+
 describe('Parent suite', () => {
+  beforeEach(skipIfPreviousTestsFailed)
+
   it('test 1', () => {})
 
-  it('test 2', () => {})
+  it('test 2', () => {
+    throw new Error('fail on purpose')
+  })
 
   it('test 3', () => {})
 })
